6b80fcf4a0e598eec1c1f28160f5b2c077415698
Kina <35324430+Kina-Nargang@users.noreply.github.com>
2020-03-23 15:26:15 -0700

    create docker compose file

--- ProductCatalogApi/Startup.cs
+++ ProductCatalogApi/Startup.cs
@@ -28,13 +28,25 @@ namespace ProductCatalogApi
         {
             services.AddControllers();
 
+            // set up for docker compose file
+
+            var server = Configuration["DatabaseServer"];
+            var database = Configuration["DatabaseName"];
+            var user = Configuration["DatabaseUser"];
+            var password = Configuration["DatabasePassword"];
+            var connectionString = 
+                $"Server={server};Database={database};User Id={user};Password={password}";
+
             // create an instance of CatalogContext, pass the options
             // options is use sqlserver and the db location is in the appsettings.json file by ConnectionString
             // here is a delegate, delegate is running a secondary thread
             // so can't call Seed() here because by the time call seed the sql service 
             // might not be ready yet
-            services.AddDbContext<CatalogContext>(options => 
-                options.UseSqlServer(Configuration["ConnectionString"]));
+            services.AddDbContext<CatalogContext>(options =>
+                // for IISExpress setting
+                //options.UseSqlServer(Configuration["ConnectionString"]));
+                // for docker
+                options.UseSqlServer(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
